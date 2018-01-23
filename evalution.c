278989a2e96c9da80620e431c2c762ece49c1292
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-01-23 16:10:53 +0530

    Create evalution.c

---
+++ evalution.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main()
+{
+   int 2, 3, 4;
+ 
+   printf("Enter two numbers to add\n");
+   scanf("%d%d",&2,&3);
+ 
+   4 = 2 + 3;
+ 
+   printf("Sum of entered numbers = %d\n",4);
+ 
+   return 0;
+}
